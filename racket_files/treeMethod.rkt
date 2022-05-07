2013de639f61e73d42f7f17521b27cbea5dd8112
Marcos Traverso <marcostraverso2003@gmail.com>
2022-05-07 18:02:36 -0400

    tree method - finally functional! TODO -> atomic vars not in lists, infix inputs

--- racket_files/treeMethod.rkt
+++ racket_files/treeMethod.rkt
@@ -3,125 +3,238 @@
 ;------------------- Structures ------------------------
 
 (define (createArgument hypotheses conclusion)
-    (list hypotheses conclusion)
+	(list hypotheses conclusion)
 )
 
 (define (createArgument2 conclusion . hypotheses)
-    (list (list conclusion) hypotheses)
+	(list (list conclusion) hypotheses)
 )
 
-(define (addHypothesis statement hypotheses )
-    (cons statement hypotheses)
+(define (addHypothesis statement hypotheses)
+	(cons statement hypotheses)
 )
 
 (define (removeHypothesis statement hypotheses)
-    (remove statement hypotheses)
+	(remove statement hypotheses)
 )
 
-;--------------- postfix Stuff (deprecated??) --------------------------------
-
-;; operators: & | ~ -> <- <->
-
-(define (getToken statement)
-   (car statement)
-)
-
-(define (removeDoubleNot statement)
-   (cond
-     [(empty? statement) '()]
-     [(empty? (cdr statement)) statement]
-     [(and (equal? (car statement) "~") (equal? (cadr statement) "~"))  (removeDoubleNot (cddr statement))]
-     [else  (cons (car statement) (removeDoubleNot (cdr statement)))]
-   )
-)
-
-(define (notOfStatement statement)
- (removeDoubleNot (append statement "~"))
-)
 ;;https://stackoverflow.com/questions/53924157/parsing-a-text-to-the-tree-in-racket-scheme
 
 ; ----------------------------------------------------------------
+
+;; operators: & | ~ -> <- <->
 
 (define (notOp) '("~"))
 (define (orOp) '("|"))
 (define (andOp) '("&"))
 (define (impOp) '("->"))
 (define (iffOp) '("<->"))
+(define (Ops) (list (orOp) (andOp) (impOp) (iffOp)))
 
 (define (matchesOperator? op)
-  (or (equal? (orOp) op)
-    (or (equal? (andOp) op)
-      (or (equal? (impOp) op)
-        (equal? (iffOp) op)
-      )
-    )
-  )
+	(and (member op (Ops)) #t)
 )
 
 (define (operable? statement)
-  (if (equal? (car statement) (notOp))
-    (operable? (cadr statement))
-    (matchesOperator? (car statement))
-  )
+	(if (equal? (car statement) (notOp))
+		(operable? (cadr statement))
+		(matchesOperator? (car statement))
+	)
 )
 
 (define (getOperable statements)
-    (if (null? statements)
-        #f
-      (if (operable? (car statements)
-        (car statements)
-        (operable? (cdr statements))
-      )
-    )
+	(if (null? statements)
+		#f
+		(if (operable? (car statements))
+			(car statements)
+			(getOperable (cdr statements))
+		)
+	)
+)
+
+(define (removeDoubleNot statement)
+	(cond
+		[(empty? statement) '()]
+		[(empty? (cdr statement)) statement]
+		[(and (equal? (car statement) (notOp)) (equal? (caadr statement) (notOp))) (removeDoubleNot (cadadr statement))]
+		[else (cons (car statement) (removeDoubleNot (cdr statement)))]
+	)
 )
 
 (define (applyNot statement)
-  (removeDoubleNot (list (notOp) statement))
+	(removeDoubleNot (list (notOp) statement))
+)
+
+
+(define (contradiction? statements)
+	(if (null? statements)
+		#f
+		(if (not (member (applyNot (car statements)) statements))
+			(contradiction? (cdr statements))
+			#t
+		)
+	)
+)
+
+; ------------------ Decomposition rules ---------------------------------------
+(define (intelligentReturn branchA branchB)
+	(cond
+		[(and branchA branchB) (list branchA branchB)]
+		[(and branchA (not branchB)) branchA]
+		[(and (not branchA) branchB) branchB]
+		[else #f]
+	)
+;	(list branchA branchB)
+)
+
+(define (handleOr statement remaining)
+	(let* (
+		      [A (cadr statement)]
+		      [B (caddr statement)]
+		      [branchA (recBrancher (cons A remaining))]
+		      [branchB (recBrancher (cons B remaining))]
+	      )
+		(intelligentReturn branchA branchB)
+	)
+)
+
+(define (handleAnd statement remaining)
+	(let* (
+		      [A (cadr statement)]
+		      [B (caddr statement)]
+	      )
+		(recBrancher (cons B (cons A remaining)))
+	)
+)
+
+(define (handleImp statement remaining)
+	(let* (
+		      [A (cadr statement)]
+		      [B (caddr statement)]
+		      [branchA (recBrancher (cons (applyNot A) remaining))]
+		      [branchB (recBrancher (cons B remaining))]
+	      )
+		(intelligentReturn branchA branchB)
+	)
+)
+
+(define (handleIff statement remaining)
+	(let* (
+		      [A (cadr statement)]
+		      [B (caddr statement)]
+		      [branchA (recBrancher (cons B (cons A remaining)))]
+		      [branchB (recBrancher (cons (applyNot B) (cons (applyNot A) remaining)))]
+	      )
+		(intelligentReturn branchA branchB)
+	)
+)
+
+(define (handleNotOr statement remaining)
+	(let (
+		     [A (cadr statement)]
+		     [B (caddr statement)]
+	     )
+		(recBrancher (cons (applyNot B) (cons (applyNot A) remaining)))
+	)
+)
+
+(define (handleNotAnd statement remaining)
+	(let* (
+		      [A (cadr statement)]
+		      [B (caddr statement)]
+		      [branchA (recBrancher (cons (applyNot A) remaining))]
+		      [branchB (recBrancher (cons (applyNot B) remaining))]
+	      )
+		(intelligentReturn branchA branchB)
+	)
+)
+(define (handleNotImp statement remaining)
+	(let (
+		     [A (cadr statement)]
+		     [B (caddr statement)]
+	     )
+		(recBrancher (cons (applyNot B) (cons A remaining)))
+	)
+)
+(define (handleNotIff statement remaining)
+	(let* (
+		      [A (cadr statement)]
+		      [B (caddr statement)]
+		      [branchA (recBrancher (cons (applyNot B) (cons A remaining)))]
+		      [branchB (recBrancher (cons B (cons (applyNot A) remaining)))]
+	      )
+		(intelligentReturn branchA branchB)
+	)
 )
 
 ;------------------- Tree Method ------------------------
 
-
-
-
-
-
-;---------------------------------------------------------
-(define (contradiciton? statements)
-  (if (null? statements)
-    #f
-    (if (not (member? (applyNot (car statements))))
-      (contradiction? (cdr statements))
-      #t
-    )
-  )
-)
-
-(define (matchAndBranch statement, remaining)
-    (if (equal? (notOp) (car statement))
-      (cond
-        [(equal? (cadar) (orOp)) (handleOR )  ]
-        [(equal? (cadar) (andOp))  ]
-        [(equal? (cadar) (impOp))  ]
-        [(equal? (cadar) (iffOp))  ]
-      )
-      (cond
-        [(equal? (cadar) (orOp)) (handleOR )  ]
-        [(equal? (cadar) (andOp))  ]
-        [(equal? (cadar) (impOp))  ]
-        [(equal? (cadar) (iffOp))  ]
-      )
-    )
+(define (matchAndBranch statement remaining) ;TODO
+	(if (equal? (notOp) (car statement))
+		(cond
+			[(equal? (cadar statement) (orOp)) (handleNotOr statement remaining)]
+			[(equal? (cadar statement) (andOp)) (handleNotAnd statement remaining)]
+			[(equal? (cadar statement) (impOp)) (handleNotImp statement remaining)]
+			[(equal? (cadar statement) (iffOp)) (handleNotIff statement remaining)]
+		)
+		(cond
+			[(equal? (car statement) (orOp)) (handleOr statement remaining)]
+			[(equal? (car statement) (andOp)) (handleAnd statement remaining)]
+			[(equal? (car statement) (impOp)) (handleImp statement remaining)]
+			[(equal? (car statement) (iffOp)) (handleIff statement remaining)]
+		)
+	)
 )
 
 
 (define (recBrancher statements)
-    (let (
-    [statement (getOperable statements)]
-    [remaining (remove statement statements)])
-        (if (statement)
-          (matchAndBranch statements remaining)
-          statements ;;no operable statements left
-        )
-    )
+	(if (contradiction? statements)
+		#f
+		(let (
+			     [statement (getOperable statements)])
+			(if statement
+				(matchAndBranch statement (remove statement statements))
+				(remove-duplicates statements) ;;no operable statements left
+			)
+		)
+	)
 )
+
+
+;--------------------Test cases ---------------------------
+
+(define (testStatement1) (list (orOp) '("A") '("B")))
+(define (testStatement2) (list (notOp) (list (orOp) '("A") '("B"))))
+(define (testStatement3) (list (notOp) '("A")))
+(define (testStatement4) '("A"))
+(define (testStatement5) (list (notOp) (list (notOp) '("A"))))
+
+(define (testHyp1) (list (testStatement1)))
+(define (testHyp2) (list (list (orOp) '("A") '("B")) (applyNot '("A"))))
+
+
+; (matchesOperator? '("&")) -> #t
+; (matchesOperator? '("aaa")) -> #f
+
+; (operable? (testStatement1)) -> #t
+; (operable? (testStatement2)) -> #t
+; (operable? (testStatement3)) -> #f
+
+; (getOperable (list (testStatement1) (testStatement2) (testStatement3) )) -> '(("|") ("A") ("B"))
+; (getOperable (list (testStatement2) (testStatement1) (testStatement3) )) -> '(("~") (("|") ("A") ("B")))
+; (getOperable (list (testStatement3) (testStatement4)) ) -> #f
+
+; (removeDoubleNot (testStatement3)) - > '(("~") ("A"))
+; (removeDoubleNot (testStatement5)) - > '("A")
+
+; (contradiction? (list (testStatement1) (applyNot (testStatement1))) ) -> #t
+; (contradiction? (list (testStatement1) (testStatement3)) ) -> #f
+
+;---------------------------------------------------------------
+
+(define (hyp1) (list (impOp) '("B") '("C")))
+(define (hyp2) (list (orOp) '("D") '("C")))
+(define (hyp3) (list (iffOp) '("B") (applyNot '("D"))))
+(define (conc) (applyNot (list (andOp) '("B") '("C"))))
+
+(define (testArgument1) (recBrancher (list (hyp1) (hyp2) (hyp3) (applyNot (conc)))))
